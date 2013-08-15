4db09035878d4c4ecbd62fae5a6c0f70fb55617d
Mason Evans <mason@mediamorph.com>
2013-08-14 21:39:03 -0400

    removed newsfeed & friendlist models

--- app/models/FriendList.scala
+++
@@ -1,9 +0,0 @@
-package models
-
-import play.api.libs.json._
-
-case class FriendList()
-
-object FriendList {
-	def getJson() = Json.stringify(Json.arr(User.findOneByUserId("1235"), User.findOneByUserId("1236")))
-}
